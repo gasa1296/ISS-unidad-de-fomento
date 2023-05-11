4cb479361d58b824cdc7b458ef959ece9239f2f4
gasa1296 <gabriel.asa.1296@outlook.es>
2023-05-11 01:23:14 -0400

    feat: added error messages to responses

--- app.py
+++ app.py
@@ -9,12 +9,9 @@ app = Flask(__name__)
 def unidad_de_fomento(year, month, day):
     #verify if valid date
     try:
-        current_date = datetime.now()
-        request_date = datetime.strptime(f'{day}-{month}-{year}', '%d-%m-%Y')
-        if current_date < request_date:
-            return '', 400
+        datetime.strptime(f'{day}-{month}-{year}', '%d-%m-%Y')
     except:
-        return '', 400
+        return {'message': 'invalid date'}, 400
     
     response = requests.get(f'https://www.sii.cl/valores_y_fechas/uf/uf{year}.htm')
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -22,17 +19,16 @@ def unidad_de_fomento(year, month, day):
     #get all months table and verify if valid
     all_table = soup.find('div', {'id':'mes_all'})
     if not all_table:
-        return '', 400
+        return {'message': 'data not found'}, 400
     
     #get all days rows and verify if valid
     day_rows = all_table.div.table.tbody.find_all('tr')
     if not day_rows or len(day_rows) < day:
-        return '', 400
+        return {'message': 'data not found'}, 400
     
     #get all months columns and verify if valid
     month_cols = day_rows[day-1].find_all('td')
     if not month_cols or len(month_cols) < month:
-        return '', 400
+        return {'message': 'data not found'}, 400
     
-    #return value
     return {'response': month_cols[month-1].get_text()}, 200
