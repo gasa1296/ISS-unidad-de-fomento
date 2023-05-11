434359c600b7ff3acd837beddf85061f50213ab7
gasa1296 <gabriel.asa.1296@outlook.es>
2023-05-11 00:06:04 -0400

    feat: returns 'unidad de forment' from date

--- app.py
+++ app.py
@@ -7,12 +7,32 @@ app = Flask(__name__)
 
 @app.route("/<int:year>/<int:month>/<int:day>")
 def unidad_de_fomento(year, month, day):
+    #verify if valid date
     try:
         current_date = datetime.now()
         request_date = datetime.strptime(f'{day}-{month}-{year}', '%d-%m-%Y')
         if current_date < request_date:
-            return {'status': 400}
+            return '', 400
     except:
-        return {'status': 500, 'message': 'invalid date'}
+        return '', 400
+    
     response = requests.get(f'https://www.sii.cl/valores_y_fechas/uf/uf{year}.htm')
     soup = BeautifulSoup(response.text, 'html.parser')
+
+    #get all months table and verify if valid
+    all_table = soup.find('div', {'id':'mes_all'})
+    if not all_table:
+        return '', 400
+    
+    #get all days rows and verify if valid
+    day_rows = all_table.div.table.tbody.find_all('tr')
+    if not day_rows or len(day_rows) < day:
+        return '', 400
+    
+    #get all months columns and verify if valid
+    month_cols = day_rows[day-1].find_all('td')
+    if not month_cols or len(month_cols) < month:
+        return '', 400
+    
+    #return value
+    return {'response': month_cols[month-1].get_text()}, 200
